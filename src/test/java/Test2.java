3e7854e48331e2a48549837a59dbe95084eec0ac
naviya <d.naviya@gmail.com>
2021-10-08 11:41:03 -0400

    resolve conflict

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,12 +1,12 @@
-public class Test3 {
-<<<<<<< HEAD
-    void test1()
-    {
+public class Test2 {
+    void test1() {
         System.out.println("hello test1");
-=======
+    }
+
+
     void test2()
     {
         System.out.println("hello test2");
->>>>>>> 1cbefb9b84826af2a474faef4a9c3ada87837c5a
+
     }
 }
